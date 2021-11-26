79e25b18863cafc7082b411258fca7f92bb38b36
Aso2001409 <2001409@s.asojuku.ac.jp>
2021-11-26 10:18:31 +0900

    test追加

--- Othubo.php
+++ Othubo.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>EC_TEST</title>
+</head>
+<body>
 <h1>大坪のひとこと</h1>
 <?php
 echo "私の趣味はチェアリングです";
 echo "一人で海辺で黄昏ています。";
 echo "さみしいです…";
 ?>
-
+</body>
+</html>
